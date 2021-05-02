1cf21a3cccb64a93fef1e09f22ed365f012240fa
Moses Cosme <mosesmaxcosme@gmail.com>
2021-05-02 06:57:16 -0500

    first commit

---
+++ src/server/knexfile.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'reactForums',
+      host: 'localhost'
+    },
+    migrations: {
+      directory: `${__dirname}/data/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/data/seeds`
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
