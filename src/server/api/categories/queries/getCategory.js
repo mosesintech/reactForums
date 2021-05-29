10906822640f274c532c8f57065f39b041842c52
Moses Cosme <mosesmaxcosme@gmail.com>
2021-05-29 09:25:03 -0500

    add nonNull

--- src/server/api/categories/queries/getCategory.js
+++ src/server/api/categories/queries/getCategory.js
@@ -1,11 +1,11 @@
-const { GraphQLID } = require('graphql');
+const { GraphQLNonNull, GraphQLID } = require('graphql');
 const { CategoryType } = require('../../../types.js');
 const { getCategory } = require('../categoriesModel.js');
 
 module.exports = {
     type: CategoryType,
     args: {
-        id: { type: GraphQLID },
+        id: { type: new GraphQLNonNull(GraphQLID) },
     },
     resolve(parents, args) {
         return getCategory(args.id);
