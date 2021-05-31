6855ebd976d62e59aefa6fa3f8d801e2431ca012
Moses Cosme <mosesmaxcosme@gmail.com>
2021-05-31 00:11:42 -0500

    add forum model

---
+++ src/server/api/forums/forumsModel.js
@@ -0,0 +1,88 @@
+const { 
+    findOne,
+    findAll,
+    findByParam,
+    addOne,
+    updateOne,
+    softDeleteOne,
+    restoreDeletedOne,
+    } = require('../../data/dataModel.js');
+
+function getForums() {
+    return findAll('forums');
+}
+
+function getForum(id) {
+    return findOne('forums', id);
+}
+
+function getForumByName(name) {
+    return findByParam('forums', { name });
+}
+
+async function addForum(forum) {
+    const nameExists = await getForumByName(forum.name);
+    if(nameExists) {
+        throw new Error('Forum name is already in use.');
+    }
+    const newForum = {
+        name: forum.name,
+        description: forum.description,
+        is_private: forum.isPrivate,
+        category_id: forum.categoryID,
+    }
+    return addOne('forums', newForum);
+}
+
+async function updateForum(forum) {
+    const idExists = await getCategory(forum.id);
+    if(!idExists) {
+        throw new Error('This forum does not exists.');
+    }
+    const identicalName = idExists.name === forum.name;
+    const identicalDescription = idExists.description === forum.description;
+    const identicalSecurity = idExists.is_private === forum.isPrivate;
+    const identicalParent = idExists.category_id === forum.categoryID;
+    const noChange = identicalName && identicalDescription && identicalSecurity && identicalParent;
+    if(noChange) {
+        throw new Error('No forum changes submitted.');
+    }
+    const updatedForum = {
+        name: forum.name,
+        description: forum.description,
+        is_private: forum.isPrivate,
+        category_id: forum.categoryID,
+    }
+    return updateOne('forums', idExists.id, updatedForum);
+}
+
+async function deleteForum(id) {
+    const idExists = await getForum(id);
+    if(!idExists) {
+        throw new Error('This forum does not exists.');
+    }
+    if(idExists.is_deleted) {
+        throw new Error('This forum is already deleted.');
+    }
+    return softDeleteOne('forums', id);
+}
+
+async function restoreForum(id) {
+    const idExists = await getForum(id);
+    if(!idExists) {
+        throw new Error('This forum does not exists.');
+    }
+    if(!idExists.is_deleted) {
+        throw new Error('This forum is not deleted.');
+    }
+    return restoreDeletedOne('forums', id);
+}
+
+module.exports = {
+    getForums,
+    getForum,
+    addForum,
+    updateForum,
+    deleteForum,
+    restoreForum,
+}
