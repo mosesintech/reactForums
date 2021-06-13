c7c459f05a49cccd6c7683bcb3aab73f4ba0ec76
Moses Cosme <mosesmaxcosme@gmail.com>
2021-06-12 23:20:38 -0500

    fix(component): remove cookie lines

--- src/components/domain/Auth/AuthLinks.jsx
+++ src/components/domain/Auth/AuthLinks.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
 
 export default function AuthLinks() {
-  const cookie = useSelector((state) => state.auth.auth);
+  // const cookie = useSelector((state) => state.auth.auth);
   return (
+    // <>
+    //   {cookie.length ? (
+    //     <>
+    //       <Link href='/logout'>Logout</Link>
+    //     </>
+    //   ) : (
     <>
-      {cookie.length ? (
-        <>
-          <Link href='/logout'>Logout</Link>
-        </>
-      ) : (
-        <>
-          <Link href='/register'>Register</Link>
-          <br />
-          <Link href='/login'>Login</Link>
-        </>
-      )}
+      <Link href='/register'>Register</Link>
+      <br />
+      <Link href='/login'>Login</Link>
     </>
+    //     )}
+    //   </>
   );
 }
